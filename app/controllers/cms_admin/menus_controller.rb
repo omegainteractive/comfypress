93a7f9b35803e1eee0f28fea653a81af884c0194
Nayeem Syed <developerinlondon@gmail.com>
2013-05-27 14:26:32 +0100

    finished menu edit

--- app/controllers/cms_admin/menus_controller.rb
+++ app/controllers/cms_admin/menus_controller.rb
@@ -1,5 +1,6 @@
 class CmsAdmin::MenusController < CmsAdmin::BaseController
   before_filter :build_menu, :only => [:new, :create]
+  before_filter :load_menu,  :only => [:edit, :update, :destroy]
 
   def index
     return redirect_to :action => :new if @site.menus.count == 0
@@ -15,6 +16,13 @@ class CmsAdmin::MenusController < CmsAdmin::BaseController
   end
 
   def create
+    @menu.save!
+    flash[:success] = I18n.t('cms.menus.created')
+    redirect_to :action => :edit, :id => @menu
+  rescue ActiveRecord::RecordInvalid
+    logger.detailed_error($!)
+    flash.now[:error] = I18n.t('cms.menus.creation_failure')
+    render :action => :new
   end
 
   def update
@@ -34,7 +42,13 @@ protected
         @menu= @site.menus.new(params[:menu])
       end
   
-
+      def load_menu
+          @menu = @site.menus.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+          flash[:error] = I18n.t('cms.menus.not_found')
+          redirect_to :action => :index
+        end
+      
 
 
 end
