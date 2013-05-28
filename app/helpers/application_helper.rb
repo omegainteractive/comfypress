ad2864140b77d6e7bf7d048aaa5431d5db795b5d
Ivan K <divout@gmail.com>
2013-05-28 22:38:18 +0600

    added cms tags for menu

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,16 @@
+module ApplicationHelper
+  def link_to_menu_item(menu_item)
+    if 'link' == menu_item.menu_item_type
+      link = link_to(menu_item.label, menu_item.link)
+    else
+      if menu_item.page_id
+        page = Cms::Page.find(menu_item.page_id)
+        link = link_to(menu_item.label, page.url)
+      else
+        ComfortableMexicanSofa.logger.warn('Page is not selected for menu item.')
+      end
+    end
+
+    link
+  end
+end
